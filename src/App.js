06ffc3c419e24fe3add18e91a6a4acae71d766a6
ECdub27 <ewiegand51@gmail.com>
2023-11-20 19:30:01 -0600

    Jammming-Project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useCallback } from 'react';
 
 function App() {
+  const [searchResults, setSearchResults] = useState('');
+  const [playlistName, setPlaylistName] =  useState([]);
+  const [playlistTrack, setPlaylistTrack] = useState('');
   return (
     <div className="App">
       
